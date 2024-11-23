63a1186e44923b1031cf7665ceda30506044211b
Cipherwill Admin <admin@cipherwill.com>
2024-11-23 13:06:58 +0530

    add: open source to homepage

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -45,10 +45,9 @@ export default function Header({
   }, []);
 
   return (
-    <>
-      <header
-        className={twMerge(
-          `fixed z-50 mx-auto w-full left-0 right-0 top-0 text-black
+    <header
+      className={twMerge(
+        `fixed z-50 mx-auto w-full left-0 right-0 top-0 text-black
           ${
             expanded
               ? twMerge(
@@ -57,101 +56,98 @@ export default function Header({
                 )
               : twMerge(`h-20`, nonExpandedClassOverride)
           } transition-all ease-in-out duration-500`,
-          classOverride
-        )}
-      >
-        {/* Important to switch theme in the middle of the page */}
-        {/* <span className="absolute top-0 opacity-0 h-0 w-0 overflow-hidden">
+        classOverride
+      )}
+    >
+      {/* Important to switch theme in the middle of the page */}
+      {/* <span className="absolute top-0 opacity-0 h-0 w-0 overflow-hidden">
           {final_state}
         </span> */}
 
-        <div className="w-full max-w-7xl mx-auto flex items-center justify-between h-full px-4">
-          <Link href="/" className="flex items-center">
-            <Image
-              id="header-logo"
-              data-cy="logo"
-              alt={`cipherwill logo`}
-              src={
-                // final_state === "dark" ? logoWhite :
-                logoBlack
-              }
-              width={150}
-              height={100}
-              priority
-            />
-          </Link>
+      <div className="w-full max-w-7xl mx-auto flex items-center justify-between h-full px-4">
+        <Link href="/" className="flex items-center">
+          <Image
+            id="header-logo"
+            data-cy="logo"
+            alt={`cipherwill logo`}
+            src={
+              // final_state === "dark" ? logoWhite :
+              logoBlack
+            }
+            width={150}
+            height={100}
+            priority
+          />
+        </Link>
 
-          <div className="hidden sm:flex items-center gap-3 -ml-6">
-            {menu.map((item) => {
-              return (
-                <Link key={item.path} href={item.path}>
-                  <div
-                    className={`mx-1 hover:text-neutral-500 p-1 font-medium
+        <div className="hidden sm:flex items-center gap-3 -ml-6">
+          {menu.map((item) => {
+            return (
+              <Link key={item.path} href={item.path}>
+                <div
+                  className={`mx-1 hover:text-neutral-500 p-1 font-medium
                   ${
                     pathname === item.path &&
                     "underline decoration-2 underline-offset-8 decoration-orange-500"
                   }
                   content-center ${expanded ? "h-16" : "h-20"}
                   `}
-                    onMouseEnter={() => {
-                      if (item.desktop_hover) {
-                        setHoverMenu(item.desktop_hover);
-                      }
-                    }}
-                    onMouseLeave={() => {
-                      if (item.desktop_hover) {
-                        setHoverMenu(null);
-                      }
-                    }}
-                  >
-                    {item.title}
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
-          <div className="flex items-center">
-            <Link href="/app">
-              <div
-                className={`
+                  onMouseEnter={() => {
+                    if (item.desktop_hover) {
+                      setHoverMenu(item.desktop_hover);
+                    }
+                  }}
+                  onMouseLeave={() => {
+                    if (item.desktop_hover) {
+                      setHoverMenu(null);
+                    }
+                  }}
+                >
+                  {item.title}
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+        <div className="flex items-center">
+          <Link href="/app">
+            <div
+              className={`
               ${
                 // final_state === "dark"
                 //   ? "bg-white text-black":
                 "bg-black text-white"
               }
               font-semibold transition-colors duration-300 px-4 py-2 rounded-full text-xs m-2`}
-              >
-                <div className="mt-[1px] sm:mt-0">
-                  {user ? "Dashboard" : "Login/Signup"}
-                </div>
+            >
+              <div className="mt-[1px] sm:mt-0">
+                {user ? "Dashboard" : "Login/Signup"}
               </div>
-            </Link>
-            <MobileHeader />
-          </div>
+            </div>
+          </Link>
+          <MobileHeader />
         </div>
+      </div>
 
-        <div
-          className={`w-fit max-w-7xl mx-auto overflow-hidden ${
-            hoverMenu !== null
-              ? "h-fit p-2 border rounded-md bg-white shadow-lg backdrop-blur-md"
-              : "h-0"
-          }`}
-          onMouseEnter={() => {
-            setHoverMenu(hoverMenu);
-          }}
-          onMouseLeave={() => {
-            setHoverMenu(null);
-          }}
-          onClick={() => {
-            setHoverMenu(null);
-          }}
-        >
-          {hoverMenu}
-          {/* <WorksMenuDesktop /> */}
-        </div>
-      </header>
-      {/* Spacer */}
-      {/* <div className={`h-20 w-full ${backdropClassOverride}`}></div> */}
-    </>
+      <div
+        className={`w-fit max-w-7xl mx-auto overflow-hidden ${
+          hoverMenu !== null
+            ? "h-fit p-2 border rounded-md bg-white shadow-lg backdrop-blur-md"
+            : "h-0"
+        }`}
+        onMouseEnter={() => {
+          setHoverMenu(hoverMenu);
+        }}
+        onMouseLeave={() => {
+          setHoverMenu(null);
+        }}
+        onClick={() => {
+          setHoverMenu(null);
+        }}
+      >
+        {hoverMenu}
+        {/* <WorksMenuDesktop /> */}
+      </div>
+    </header>
   );
 }
--- components/public/DeadManSwitchExplainer.tsx
+++ components/public/DeadManSwitchExplainer.tsx
@@ -1,19 +1,30 @@
 "use client";
+import { RiOpenSourceFill } from "react-icons/ri";
+import { ImSwitch } from "react-icons/im";
 import Popup from "reactjs-popup";
+import { GiPadlock } from "react-icons/gi";
 
 export default function DeadManSwitchExplainer() {
   return (
     <>
       <div className="bg-gradient-to-b from-neutral-800 to-black text-white font-black py-10 overflow-hidden">
-        <div className="text-2xl sm:text-5xl md:text-6xl xl:text-7xl w-full max-w-7xl mx-auto p-2">
-          <div className="text-end w-full whitespace-nowrap">
+        <div className="flex flex-col gap-2 text-2xl sm:text-5xl md:text-6xl xl:text-7xl w-full max-w-7xl mx-auto p-2">
+          <div className="flex items-center gap-2">
+            <RiOpenSourceFill className="scale-75" />
+            OPEN SOURCE
+          </div>
+          <div className="flex items-center gap-2">
+            <GiPadlock className="scale-75" />
             END-TO-END ENCRYPTED
           </div>
-          <div className="text-start w-full whitespace-nowrap">
-            DEAD MAN&apos;S SWITCH
+          <div className="flex flex-col md:flex-row gap-2 justify-between md:items-end">
+            <div className="flex items-center gap-2">
+              <ImSwitch className="scale-75" />
+              DEAD MAN&apos;S SWITCH
+            </div>
             <Popup
               trigger={
-                <button className="border font-medium border-neutral-400 hover:bg-neutral-800 rounded-full text-sm px-4 py-1 mx-2">
+                <button className="border font-medium border-neutral-400 hover:bg-neutral-800 rounded-full text-sm px-4 py-1 mx-2 h-min">
                   What is that?
                 </button>
               }
