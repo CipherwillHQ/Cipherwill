98644f63d51d573c30b7781caa91244eeb6f4b47
Cipherwill Admin <admin@cipherwill.com>
2024-12-04 15:32:43 +0530

    fix: update Terms of Service

--- app/i/terms-of-service/page.tsx
+++ app/i/terms-of-service/page.tsx
@@ -25,7 +25,7 @@ export default function TOS() {
       <Header />
       <PolicyPageHeader title={`Terms of\nService`} />
       <div className="max-w-4xl mx-auto py-10 text-justify p-4 font-medium text-lg">
-        <div className="py-2">Effective Date : 20 May 2024</div>
+        <div className="py-4">Last updated on December 3, 2024</div>
         <div>
           These Terms of Use (&quot;Terms&quot;) govern your access to and use
           of the CipherWill platform and services (&quot;Services&quot;). By
@@ -34,7 +34,7 @@ export default function TOS() {
           Services.
         </div>
 
-        <h2 className="py-2 font-semibold">1. Eligibility</h2>
+        <h2 className="pt-12 pb-4 font-semibold">1. Eligibility</h2>
         <div>
           By using our Services, you represent and warrant that you meet these
           eligibility requirements.
@@ -62,7 +62,9 @@ export default function TOS() {
           </ul>
         </div>
 
-        <h2 className="py-2 font-semibold">2. Account Creation and Security</h2>
+        <h2 className="pt-12 pb-4 font-semibold">
+          2. Account Creation and Security
+        </h2>
         <div>
           2.1 Account Creation: In order to access certain features of our
           Services, you may be required to create an account. You agree to
@@ -79,7 +81,7 @@ export default function TOS() {
           unauthorized use or suspected breach of security.
         </div>
 
-        <h2 className="py-2 font-semibold">3. Use of Services</h2>
+        <h2 className="pt-12 pb-4 font-semibold">3. Use of Services</h2>
         <div>
           3.1 License: Subject to your compliance with these Terms, we grant you
           a limited, non-exclusive, non-transferable, and revocable license to
@@ -89,17 +91,22 @@ export default function TOS() {
           <br />
           3.2 Prohibited Conduct: You agree not to engage in any of the
           following prohibited activities:
-          <br />a{")"} Violating any applicable laws, regulations, or
+          br
+          <br />3.2.1{")"} Distributing, modifying, or using our open-source code
+          for commercial purposes is strictly prohibited. The code may only be
+          used for personal, non-commercial purposes, and may not be sold,
+          redistributed, or altered for commercial gain.
+          <br />3.2.2{")"} Violating any applicable laws, regulations, or
           third-party rights;
-          <br />b{")"} Using our Services for any unlawful, fraudulent, or
+          <br />3.2.3{")"} Using our Services for any unlawful, fraudulent, or
           malicious purposes;
-          <br />c{")"} Interfering with or disrupting the integrity, security,
+          <br />3.2.4{")"} Interfering with or disrupting the integrity, security,
           or performance of our Services;
-          <br />d{")"} Accessing or attempting to access the accounts, systems,
+          <br />3.2.5{")"} Accessing or attempting to access the accounts, systems,
           or networks of other users without authorization;
-          <br />e{")"} Uploading, transmitting, or distributing any viruses,
+          <br />3.2.6{")"} Uploading, transmitting, or distributing any viruses,
           malware, or other harmful content;
-          <br />f{")"} Engaging in any activity that could damage our reputation
+          <br />3.2.7{")"} Engaging in any activity that could damage our reputation
           or the reputation of our Services.
           <br />
           <br />
@@ -110,7 +117,9 @@ export default function TOS() {
           display, and distribute your content for the purposes of providing and
           promoting our Services.
         </div>
-        <h2 className="py-2 font-semibold">4. Intellectual Property Rights</h2>
+        <h2 className="pt-12 pb-4 font-semibold">
+          4. Intellectual Property Rights
+        </h2>
         <div>
           All intellectual property rights in our Services, including but not
           limited to copyrights, trademarks, and trade secrets, belong to
@@ -119,17 +128,7 @@ export default function TOS() {
           our prior written permission.
         </div>
 
-        <h2 className="py-2 font-semibold">5. Disclaimer of Warranties</h2>
-        <div>
-          Our Services are provided on an &quot;as is&quot; and &quot;as
-          available&quot; basis, without any warranties or representations of
-          any kind, whether express or implied. We do not guarantee the
-          accuracy, completeness, or reliability of any information or content
-          provided through our Services. Your use of our Services is at your own
-          risk.
-        </div>
-
-        <h2 className="py-2 font-semibold">6. Limitation of Liability</h2>
+        <h2 className="pt-12 pb-4 font-semibold">5. Limitation of Liability</h2>
         <div>
           To the maximum extent permitted by applicable law, CipherWill and its
           affiliates, directors, officers, employees, agents, and licensors
@@ -139,14 +138,14 @@ export default function TOS() {
           possibility of such damages.
         </div>
 
-        <h2 className="py-2 font-semibold">7. Indemnification</h2>
+        <h2 className="pt-12 pb-4 font-semibold">6. Indemnification</h2>
         <div>
           You agree to indemnify and hold CipherWill and its affiliates,
           directors, officers, employees, agents, and licensors harmless from
           any claims, losses, damages, liabilities, costs, and expenses,
           including reasonable attorneys&apos; fees, arising out of or in
           connection with your use of our Services, your violation of these
-          Terms, or your violation of any rights
+          Terms, or your violation of any rights.
         </div>
       </div>
       <Footer />
