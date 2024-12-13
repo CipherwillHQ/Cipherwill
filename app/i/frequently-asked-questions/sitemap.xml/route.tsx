be7b95b03f82acd911369550500d3e31efa9bcdf
Cipherwill Admin <admin@cipherwill.com>
2024-12-14 04:36:08 +0530

    fix: update FAQ sitemap generation to use async iteration for improved performance

--- app/i/frequently-asked-questions/sitemap.xml/route.tsx
+++ app/i/frequently-asked-questions/sitemap.xml/route.tsx
@@ -6,8 +6,8 @@ export async function GET(request: Request) {
   const faqs = questions;
   const lastmod = DateTime.now().startOf("month").toISO();
   const urls = [];
-  for (const [key, value] of Object.entries(faqs)) {
-    for (const [_, faq] of Object.entries(value)) {
+  for await (const [key, value] of Object.entries(faqs)) {
+    for await (const [_, faq] of Object.entries(value)) {
       urls.push({
         loc: `https://www.cipherwill.com/i/frequently-asked-questions/${faq.slug}`,
         lastmod,
