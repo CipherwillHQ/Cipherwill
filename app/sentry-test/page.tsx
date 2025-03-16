f8ac01dd73254d1e87b5578cb51ca668196fcfb0
Cipherwill Admin <admin@cipherwill.com>
2025-03-16 12:03:28 +0530

    feat: add Sentry test page with error throwing functionality

---
+++ app/sentry-test/page.tsx
@@ -0,0 +1,14 @@
+export default function SentryTestPage() {
+  return (
+    <div>
+      <h1>Sentry Test Page</h1>
+      <button
+        onClick={() => {
+          throw new Error("Sentry Test Error");
+        }}
+      >
+        Throw Error
+      </button>
+    </div>
+  );
+}
