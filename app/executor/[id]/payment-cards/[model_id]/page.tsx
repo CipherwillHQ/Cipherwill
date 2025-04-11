9ea287ceaac45c857f7fe73e00dda907d6ce6590
Cipherwill Admin <admin@cipherwill.com>
2025-04-12 02:54:05 +0530

    feat: implement donor note and payment card views with decryption logic and loading states

---
+++ app/executor/[id]/payment-cards/[model_id]/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useQuery } from "@apollo/client";
+import getTimeAgo from "../../../../../common/time/getTimeAgo";
+import { useState } from "react";
+import GET_GRANTED_METAMODEL from "../../../../../graphql/ops/app/executor/metamodels/GET_GRANTED_METAMODEL";
+import { useParams } from "next/navigation";
+import useDecryptedPod from "@/common/executor/hooks/useDecryptedPod";
+import { PAYMENT_CARD_TYPE } from "@/types/pods/PAYMENT_CARD";
+
+export default function DonorNoteView() {
+  const params = useParams();
+  const access_id: string = params.id as string;
+  const payment_card_id: string = params.model_id as string;
+
+  const [decryptedValue, setDecryptedValue] =
+    useState<PAYMENT_CARD_TYPE | null>(null);
+
+  const [keyMetadata, setKeyMetadata] = useState(null);
+  useDecryptedPod({
+    access_id,
+    metamodel_id: payment_card_id,
+    setData(data) {
+      setDecryptedValue(data);
+    },
+    setKeyMetadata(data) {
+      setKeyMetadata(data);
+    },
+  });
+
+  const { data: granted_metamodel } = useQuery(GET_GRANTED_METAMODEL, {
+    variables: {
+      access_id,
+      model_id: payment_card_id,
+    },
+  });
+  if (!granted_metamodel) return <div>Loading Granted Models...</div>;
+
+  const parsed_data = JSON.parse(
+    granted_metamodel.getGrantedMetamodel.metadata
+  );
+
+  return (
+    <div className="w-full">
+      <div className="p-2">
+        Title : {parsed_data.name || "Untitled"}
+        <br />
+        {keyMetadata && (
+          <div>
+            Created At: {getTimeAgo(parseInt(keyMetadata.created_at))}
+            <br />
+            Updated At : {getTimeAgo(parseInt(keyMetadata.updated_at))}
+          </div>
+        )}
+        <div className="border-b my-2" />
+        Content :
+        <br />
+        {decryptedValue != null && (
+          <div className="whitespace-pre-line bg-secondary p-2">
+            {JSON.stringify(decryptedValue, null, 2)}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
