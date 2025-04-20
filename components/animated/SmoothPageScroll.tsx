0bd8b9e4f0c65ab6c0c5dc0ae8dcc719401e8044
Cipherwill Admin <admin@cipherwill.com>
2025-04-21 04:25:52 +0530

    refactor: comment out Lenis implementation in SmoothPageScroll to resolve scroll position issues

--- components/animated/SmoothPageScroll.tsx
+++ components/animated/SmoothPageScroll.tsx
@@ -2,24 +2,26 @@
 import Lenis from "lenis";
 import { useEffect } from "react";
 
+//TODO: Removed lenis to avoid issue with Scroll Position in Link component
+
 export default function SmoothPageScroll() {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.5, // adjust as needed
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    });
+  // useEffect(() => {
+  //   const lenis = new Lenis({
+  //     duration: 1.5, // adjust as needed
+  //     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  //   });
 
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+  //   function raf(time: number) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
 
-    requestAnimationFrame(raf);
+  //   requestAnimationFrame(raf);
 
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+  //   return () => {
+  //     lenis.destroy();
+  //   };
+  // }, []);
 
   return null;
 }
