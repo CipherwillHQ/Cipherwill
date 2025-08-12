0915867df6b330a8085479117a03c216415e2124
Cipherwill Admin <admin@cipherwill.com>
2025-08-12 17:50:52 +0530

    feat: add account reset and deletion functionality with confirmation prompts

--- app/app/profile/page.tsx
+++ app/app/profile/page.tsx
@@ -3,12 +3,18 @@ import Profile from "./Profile";
 import { useAuth } from "@/contexts/AuthContext";
 import DesktopAndMobilePageHeader from "@/components/app/common/page/DesktopAndMobilePageHeader";
 import MobileMenu from "@/components/app/Sidebar/MobileMenu";
+import DELETE_DATA from "@/graphql/ops/auth/mutations/DELETE_DATA";
+import DELETE_ACCOUNT from "@/graphql/ops/auth/mutations/DELETE_ACCOUNT";
+import { useMutation } from "@apollo/client";
+import ConfirmationButton from "@/components/common/ConfirmationButton";
 
 export default function ProfilePage() {
   const { logout } = useAuth();
+  const [deleteData] = useMutation(DELETE_DATA);
+  const [deleteAccount] = useMutation(DELETE_ACCOUNT);
   return (
     <div className="w-full">
-      <MobileMenu home/>
+      <MobileMenu home />
       <DesktopAndMobilePageHeader title="Profile">
         <button
           className="border border-red-500 hover:bg-red-100 text-red-500 hover:text-red-700 py-1 px-4 text-sm rounded-md"
@@ -33,6 +39,47 @@ export default function ProfilePage() {
             >
               Logout
             </button>
+          </div>
+        </div>
+
+        <div className="max-w-3xl bg-secondary p-4 rounded-md border border-default">
+          <div className="text-lg font-medium pb-2">Reset account</div>
+          <div className="flex flex-col sm:flex-row sm:items-center justify-between">
+            <div className="text-sm pb-4 max-w-md">
+              This will delete all your account data and log you out. You will
+              need to log in again to access your account. Are you sure you want
+              to delete all your account data?
+            </div>
+            <ConfirmationButton
+              onConfirm={() => {
+                deleteData().then(() => {
+                  logout();
+                });
+              }}
+              className="border border-red-500 hover:bg-red-100 text-red-500 hover:text-red-700 px-8 py-1 rounded-md"
+            >
+              Reset account
+            </ConfirmationButton>
+          </div>
+        </div>
+
+        <div className="max-w-3xl bg-secondary p-4 rounded-md border border-default">
+          <div className="text-lg font-medium pb-2">Delete account</div>
+          <div className="flex flex-col sm:flex-row sm:items-center justify-between">
+            <div className="text-sm pb-4 max-w-md">
+              This will delete your account and all associated data. Are you
+              sure you want to delete your account?
+            </div>
+            <ConfirmationButton
+              onConfirm={() => {
+                deleteAccount().then(() => {
+                  logout();
+                });
+              }}
+              className="border border-red-500 hover:bg-red-100 text-red-500 hover:text-red-700 px-8 py-1 rounded-md"
+            >
+              Delete account
+            </ConfirmationButton>
           </div>
         </div>
       </div>
