f81699e5653b5307bfd620b582fc0d72bec6b583
Cipherwill Admin <admin@cipherwill.com>
2025-08-16 17:39:02 +0530

    fix: implement pagination for Notion posts retrieval in sitemap generation

--- app/blogs-sitemap.xml/route.tsx
+++ app/blogs-sitemap.xml/route.tsx
@@ -1,12 +1,32 @@
 import { getServerSideSitemap } from "next-sitemap";
 import { getNotionPosts } from "../blog/getNotionPosts";
 
-export async function GET(request: Request) {
-  const { pages } = await getNotionPosts({
-    size: 100,
-  })
+const PAGE_SIZE = 100;
 
-  const urls = pages.map((page: any) => {
+export async function GET(request: Request) {
+  let thats_all = false;
+  let all_pages: any[] = [];
+
+  while (!thats_all) {
+    const cursor =
+      all_pages.length > 0 ? all_pages[all_pages.length - 1].id : undefined;
+
+    const { pages } = await getNotionPosts({
+      cursor,
+      size: PAGE_SIZE,
+    });
+    if (cursor) {
+      // skip first page as it is the cursor page from last results
+      all_pages = all_pages.concat(pages.slice(1));
+    } else {
+      all_pages = all_pages.concat(pages);
+    }
+
+    if (pages.length < PAGE_SIZE) {
+      thats_all = true;
+    }
+  }
+  const urls = all_pages.map((page: any) => {
     const slug = page.properties.slug.rich_text[0].text.content;
     return {
       loc: `https://www.cipherwill.com/blog/${slug}-${page.id.replaceAll(
