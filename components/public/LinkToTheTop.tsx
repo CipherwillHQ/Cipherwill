1f9c1f4df2621236492e1cee9bf37d03248608ce
Cipherwill Admin <admin@cipherwill.com>
2025-09-02 23:15:15 +0530

    feat: refactor LinkToTheTop component to use Next.js Link for improved navigation

--- components/public/LinkToTheTop.tsx
+++ components/public/LinkToTheTop.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
 export default function LinkToTheTop({
   href,
   children,
@@ -7,9 +13,28 @@ export default function LinkToTheTop({
   children: React.ReactNode;
   className?: string;
 }) {
+  const pathname = usePathname();
+  useEffect(() => {
+    if (pathname === href) {
+      // If the current pathname matches the link's href, scroll to the top
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, href]);
+
   return (
-    <a href={href} className={className}>
+    <Link href={href} className={className}>
       {children}
-    </a>
+    </Link>
+  );
+
+  return (
+    <button
+      className={`hover:cursor-pointer ${className}`}
+      onClick={() => {
+        window.location.assign(href);
+      }}
+    >
+      {children}
+    </button>
   );
 }
