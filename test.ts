a7bb13e97ff971dbf0eb157fea75557fbfbcb7e1
Cipherwill Admin <admin@cipherwill.com>
2025-11-27 14:55:16 +0530

    fix: remove trailing whitespace in test.ts

--- test.ts
+++ test.ts
@@ -5,5 +5,5 @@ import { NotionAPI } from "notion-client";
     const id = "23f6d63626188021a7f6c53e8651252f"; // Replace with your actual blog post ID\
     const recordMap = await notion.getPage(id);
     console.log(`Fetched record map for post ID: ${id}`, recordMap);
-    
+  
 })();
