fda48f43e9f8fa880615c23b3284c68cd1ebc914
Cipherwill Admin <admin@cipherwill.com>
2026-03-13 16:22:23 +0530

    Hide guided actions until objective exists

--- components/app/GuidedActions/index.tsx
+++ components/app/GuidedActions/index.tsx
@@ -105,7 +105,7 @@ export default function GuidedActions() {
 
   useBodyOverflowLock(showGuidedActions);
 
-  if (!hasObjective && !loading && !error) {
+  if (!hasObjective) {
     return null;
   }
 
