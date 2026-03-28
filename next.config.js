b0c20c36dcc836737cdf460f64c97f1849e5c8b3
Cipherwill Admin <admin@cipherwill.com>
2026-03-28 13:32:14 +0530

    Remove blog sitemap generation and proxy /blogs-sitemap.xml

--- next.config.js
+++ next.config.js
@@ -59,6 +59,10 @@ const nextConfig = {
   },
   async rewrites() {
     return [
+      {
+        source: "/blogs-sitemap.xml",
+        destination: "https://media.cipherwill.com/sitemap/blogs-sitemap.xml",
+      },
       // Using custom endpoint for PostHog
       // do not need this
 
